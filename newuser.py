cec25a9998ce9a6a9311aaef9d32c0a41b9ab9e0
Anilam <troyanka-girl@yandex.ru>
2016-02-16 17:01:24 +0100

    123

---
+++ newuser.py
@@ -0,0 +1,21 @@
+import crypt
+import pwd
+import os
+def createUser(name, username, password):
+	counter = 0;
+	_username = username;
+	flag = 0;
+	while 1:	
+		try: 
+			if flag == 1:
+				break;
+			pwd.getpwnam(_username)
+			_username = username + str(counter);			
+			counter = counter+1;
+		except KeyError:
+			flag = 1;
+	encPass = crypt.crypt(password,"22");
+	print('useradd '+_username+' -p ' + encPass + ' -d ' + '/home/'+_username +'/'+ ' -m ');
+	return os.system('useradd '+_username+' -p ' + encPass + ' -d ' + '/home/'+_username +'/'+ ' -m ');
+
+createUser('dante', 'dante', '123');
